1658946f4c942a51b0ec2f04e1b6a650950a2a89
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-10-23 03:05:30 +0600

    Update AminoLikeFxck.py

--- AminoLikeFxck.py
+++ AminoLikeFxck.py
@@ -4,8 +4,8 @@ import concurrent.futures
 from colored import fore, back, style, attr
 attr(0)
 print(fore.CADET_BLUE_1 + style.BOLD)
-print("""Script by Lil Zevi
-Github : https://github.com/LilZevi""")
+print("""Script by deluvsushi
+Github : https://github.com/deluvsushi""")
 print(pyfiglet.figlet_format("Aminolikefxck", font="fourtops"))
 client = AminoLab.Client()
 email = input("Email >> ")
