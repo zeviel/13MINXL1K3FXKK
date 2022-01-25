9c0b15c23cd149e5393f2cc921696033db10f776
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-25 12:18:35 +0600

    Update AminoLikeFxck.py

--- AminoLikeFxck.py
+++ AminoLikeFxck.py
@@ -1,31 +1,32 @@
-import AminoLab
-import pyfiglet
-import concurrent.futures
-from colored import fore, back, style, attr
+import amino
+from pyfiglet import figlet_format
+from colored import fore, style, attr
+from concurrent.futures import ThreadPoolExecutor
 attr(0)
-print(fore.CADET_BLUE_1 + style.BOLD)
-print("""Script by deluvsushi
-Github : https://github.com/deluvsushi""")
-print(pyfiglet.figlet_format("Aminolikefxck", font="fourtops"))
-client = AminoLab.Client()
-email = input("Email >> ")
-password = input("Password >> ")
-client.auth(email=email, password=password)
-clients = client.my_communities()
+print(
+    f"""{fore.CADET_BLUE_1 + style.BOLD}
+Script by deluvsushi
+Github : https://github.com/deluvsushi"""
+)
+print(figlet_format("Aminolikefxck", font="fourtops"))
+client = amino.Client()
+email = input("-- Email::: ")
+password = input("-- Password::: ")
+client.login(email=email, password=password)
+clients = client.sub_clients(start=0, size=100)
 for x, name in enumerate(clients.name, 1):
     print(f"{x}.{name}")
-ndc_Id = clients.ndc_Id[int(input("Select the community >> ")) - 1]
-
-blogs_count = int(input("Blogs Count >> "))
+com_id = clients.comId[int(input("-- Select the community::: ")) - 1]
+sub_client = amino.SubClient(comId=com_id, profile=client.profile)
 
 while True:
     try:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-            for i in range(0, 20000, 250):
-                recent_blogs = client.get_recent_blogs(
-                    ndc_Id=ndc_Id, start=i, size=blogs_count).blog_Id
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            for i in range(0, 2500, 15000):
+                recent_blogs = sub_client.get_recent_blogs(
+                    start=i, size=100).blog_Id
                 for blog_id in recent_blogs:
-                    _ = [executor.submit(client.vote, ndc_Id, blog_id)]
-                    print(f"Liked >> {blog_id}")
+                    executor.submit(client.like, blog_id)
+                    print(f"-- Liked::: {blog_id}")
     except Exception as e:
         print(e)
